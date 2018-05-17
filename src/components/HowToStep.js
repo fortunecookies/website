5229e2a8b7c5c8b71e8e23b66a2373d8dc1127e3
Alexander Haniotis <alexanderhaniotis@gmail.com>
2018-05-17 10:07:17 -0400

    HowTo

---
+++ src/components/HowToStep.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { node, string } from 'prop-types'
+import styled, { css } from 'styled-components'
+import { stripUnit, rgba } from 'polished'
+
+import * as spacing from 'styles/spacing'
+import Icon from 'components/Icon'
+import { greyDark, white, borderColor, primaryColor } from 'styles/colors'
+import { media } from 'styles/media'
+import { H3, fontBold } from 'styles/typography'
+
+const Container = styled.div`
+  position: relative;
+  display: block;
+  text-align: center;
+  width: 100%;
+  margin-bottom: ${spacing.medium};
+
+  ${media.medium`
+    margin-bottom: ${spacing.large};
+  `};
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+
+const Content = styled.div`
+  border: 1px solid ${rgba(primaryColor, 0.3)};
+  padding: ${spacing.large};
+  padding-top: ${spacing.xxLarge};
+
+  ${media.medium`
+    padding: ${spacing.xLarge};
+    padding-top: 48px;
+  `};
+
+  ${media.large`
+    padding: ${spacing.xxLarge};
+    padding-top: ${spacing.xxxLarge};
+  `};
+`
+
+const Step = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${white};
+  font-weight: ${fontBold};
+  width: 64px;
+  height: 48px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: -${spacing.large};
+  background-position: center;
+  background-size: 100%;
+  background-image: url(${require('static/images/step.png')});
+
+  ${media.medium`
+    width: 88px;
+    height: 66px;
+    margin-bottom: -33px;
+  `};
+`
+
+const HowToStep = ({ children }) => (
+  <Container>
+    <Step>1</Step>
+    <Content>
+      {children}
+    </Content>
+  </Container>
+)
+
+HowToStep.propTypes = {
+  children: node.isRequired
+}
+
+export default HowToStep
--- src/views/HowToPlay.js
+++ src/views/HowToPlay.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
+import { H3 } from 'styles/typography'
 import Header from 'components/Header'
 import Wrapper from 'components/Wrapper'
-import Section from 'components/Section'
+import Section, { SectionBlock } from 'components/Section'
+import HowToStep from 'components/HowToStep'
 
 const HowToPlay = () => (
   <Fragment>
@@ -13,9 +15,22 @@ const HowToPlay = () => (
         <span>Batches of new Fortune Cookies are sold in Bake Sales. To participate in a Bake Sale, head to <Link to="/bake-sale">fortunecookies.io/bakesale</Link> and follow the instructions on screen. Here are some addition tips for a successfull Bake Sale.</span>
       }
     />
-    <Section flushTop>
+    <Section flushTop center>
       <Wrapper narrow>
-        How to goes here
+        <SectionBlock>
+          <H3>Rules of the Bake Sale</H3>
+        </SectionBlock>
+        <SectionBlock>
+          <HowToStep>
+            <p>Follow the official FortuneCookies Twitter, Telegram, and Medium accounts to stay informed on when new batches will go on sale, and other details about the batch. We will announce the start of the Bake Sale a week before the launch on all official channels.</p>
+          </HowToStep>
+          <HowToStep>
+            <p>Bake Sales begin at a pre-announced block number and go live when the Bake Sale page on our website becomes active.</p>
+          </HowToStep>
+          <HowToStep>
+            <p>Individual FortuneCookie auctions begin at a high price and ends at a low price. Each individual auction lasts 30 minutes, or until the FortuneCookie is purchased — whichever comes first.</p>
+          </HowToStep>
+        </SectionBlock>
       </Wrapper>
     </Section>
   </Fragment>
