ea08bdeb78c285c5799fd23e55ad96221d78180c
Alexander Haniotis <alexanderhaniotis@gmail.com>
2018-05-25 10:40:14 -0400

    Remove Typekit script

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Switch } from 'react-router'
 import { Redirect } from 'react-router-dom'
 import { AsyncRoute } from 'layouts/asyncComponents'
-import Typekit from 'react-typekit'
-import { Portal } from 'react-portal'
 
 import Nav from 'components/Nav'
 import Footer from 'components/Footer'
@@ -21,9 +19,6 @@ const Layout = () => (
     </Switch>
     <Footer />
     <MobileNav />
-    <Portal>
-      <Typekit kitId="eun2pom" />
-    </Portal>
   </>
 )
 
