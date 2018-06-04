67c2cedb5d091f221c47b87d6737879595733fce
Alexander Haniotis <alexanderhaniotis@gmail.com>
2018-06-04 15:22:25 -0400

    Only apply theme on bake sale page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { object, string } from 'prop-types'
-import { Switch } from 'react-router'
+import { object } from 'prop-types'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider, connect } from 'react-redux'
 import { PersistGate } from 'redux-persist/es/integration/react'
-import { ThemeProvider } from 'styled-components'
 
 import Layout from 'layouts/Layout'
 
-const mapStateToProps = ({ store }) => ({
+const mapStateToProps = ({ store }, props) => ({
   storeStatus: store.status
 })
 
@@ -16,29 +14,19 @@ const mapStateToProps = ({ store }) => ({
 export default class App extends Component {
   static propTypes = {
     store: object.isRequired,
-    persistor: object.isRequired,
-    storeStatus: string.isRequired
+    persistor: object.isRequired
   };
 
   render() {
-    const { store, persistor, storeStatus } = this.props
-    const isStoreClosed = storeStatus === 'closed'
-    console.log(isStoreClosed)
+    const { store, persistor } = this.props
+
     return (
       <Provider store={store}>
-        <ThemeProvider
-          theme={{
-            isStoreClosed
-          }}
-        >
-          <PersistGate persistor={persistor}>
-            <Router>
-              <Switch>
-                <Route path="/" component={Layout} />
-              </Switch>
-            </Router>
-          </PersistGate>
-        </ThemeProvider>
+        <PersistGate persistor={persistor}>
+          <Router>
+            <Route path="/" component={Layout} />
+          </Router>
+        </PersistGate>
       </Provider>
     )
   }
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
-import { object } from 'prop-types'
+import { object, string } from 'prop-types'
 import { Switch } from 'react-router'
+import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { AsyncRoute } from 'layouts/asyncComponents'
+import { ThemeProvider } from 'styled-components'
 
 import Nav from 'components/Nav'
 import Page from 'components/Page'
 import Footer from 'components/Footer'
 import MobileNav from 'components/MobileNav'
 
+const mapStateToProps = ({ store }, { location }) => ({
+  storeStatus: store.status,
+  location
+})
+
+@connect(mapStateToProps)
 class Layout extends Component {
   static propTypes = {
-    location: object.isRequired
+    location: object.isRequired,
+    storeStatus: string.isRequired
   };
 
   componentDidUpdate(prevProps) {
@@ -21,23 +30,28 @@ class Layout extends Component {
   }
 
   render() {
+    const { location, storeStatus } = this.props
+    console.log(location)
+    const isStoreClosed = location.pathname.includes('bake-sale') && storeStatus === 'closed'
     return (
-      <Page>
-        <Nav />
-        <Switch>
-          <AsyncRoute exact path="/" loader={() => import('views/Home')} />
-          <AsyncRoute exact path="/faqs/" loader={() => import('views/Faqs')} />
-          <AsyncRoute exact path="/how-to-play/" loader={() => import('views/HowToPlay')} />
-          <AsyncRoute exact path="/bake-sale/" loader={() => import('views/BakeSale')} />
-          <AsyncRoute exact path="/my-account/" loader={() => import('views/MyAccount')} />
-          <AsyncRoute exact path="/terms/" loader={() => import('views/Terms')} />
-          <AsyncRoute exact path="/privacy-policy/" loader={() => import('views/Privacy')} />
-          <AsyncRoute exact path="/help/" loader={() => import('views/Help')} />
-          <Redirect to="/" />
-        </Switch>
-        <Footer />
-        <MobileNav />
-      </Page>
+      <ThemeProvider theme={{ isStoreClosed }}>
+        <Page>
+          <Nav />
+          <Switch>
+            <AsyncRoute exact path="/" loader={() => import('views/Home')} />
+            <AsyncRoute exact path="/faqs/" loader={() => import('views/Faqs')} />
+            <AsyncRoute exact path="/how-to-play/" loader={() => import('views/HowToPlay')} />
+            <AsyncRoute exact path="/bake-sale/" loader={() => import('views/BakeSale')} />
+            <AsyncRoute exact path="/my-account/" loader={() => import('views/MyAccount')} />
+            <AsyncRoute exact path="/terms/" loader={() => import('views/Terms')} />
+            <AsyncRoute exact path="/privacy-policy/" loader={() => import('views/Privacy')} />
+            <AsyncRoute exact path="/help/" loader={() => import('views/Help')} />
+            <Redirect to="/" />
+          </Switch>
+          <Footer />
+          <MobileNav />
+        </Page>
+      </ThemeProvider>
     )
   }
 }
